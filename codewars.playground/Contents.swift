45de01fc23195c95cdb8ec245b3960e3ccde58cc
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-22 09:32:45 -0600

    kata level 8 solved

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -182,3 +182,11 @@ extension Array {
 }
 
 print("ABC".findPermutations())
+
+//Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+//
+//For example, a multiplication table (string) for number == 5 looks like below:
+
+func multi_table(_ number: Int) -> String {
+    return "1 * \(number) = \(number * 1)\n2 * \(number) = \(number * 2)\n3 * \(number) = \(number * 3)\n4 * \(number) = \(number * 4)\n5 * \(number) = \(number * 5)\n6 * \(number) = \(number * 6)\n7 * \(number) = \(number * 7)\n8 * \(number) = \(number * 8)\n9 * \(number) = \(number * 9)\n10 * \(number) = \(number * 10)"
+}
