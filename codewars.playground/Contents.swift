4b393f6da65e9c61031da907ada75013e851f27f
James L <59751464+Jazz1016@users.noreply.github.com>
2021-08-25 09:27:23 -0600

    kata lvl 7 completed

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -721,5 +721,21 @@ func digits(num n: UInt64) -> Int {
     return "\(n)".count
 }
 
-digits(num: 12345)
-digits(num: 9876543210)
+//digits(num: 12345)
+//digits(num: 9876543210)
+
+var tupleTest = (1, "Test", 2.3)
+
+tupleTest.1
+
+//Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+//
+//Example:
+//
+//Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+func flattenAndSort<T: Comparable>(_ arr: [[T]]) -> [T] {
+    return arr.flatMap { $0 }.sorted()
+}
+
+//flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]])
