81d915ff130cffdb7917b9eb19db64d72f6c09ca
James L <59751464+Jazz1016@users.noreply.github.com>
2021-12-04 23:43:55 -0700

    kata lvl 7

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1413,10 +1413,10 @@ switchItUp(1)
     
 //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-func simpleMultiplication(_ num: Int) -> Int {
-  if num % 2 == 0 {
-    return num * 8
-  } else {
-    return num * 9
-  }
-}
+//func simpleMultiplication(_ num: Int) -> Int {
+//  if num % 2 == 0 {
+//    return num * 8
+//  } else {
+//    return num * 9
+//  }
+//}
