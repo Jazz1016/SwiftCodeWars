c66733ed948dfd0b0e82a58c5e45767778f78241
James L <59751464+Jazz1016@users.noreply.github.com>
2021-12-07 09:29:49 -0700

    kata lvl 8 completed

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1420,3 +1420,29 @@ switchItUp(1)
 //    return num * 9
 //  }
 //}
+
+
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+//
+//Task
+//Given a year, return the century it is in.
+//
+//Examples
+//1705 --> 18
+//1900 --> 19
+//1601 --> 17
+//2000 --> 20
+
+func century(_ year: Int) -> Int {
+    var num = year/100
+    
+    if year % 100 == 0 {
+        return num
+    } else {
+        return num + 1
+    }
+}
+
+century(89)
+century(1900)
+century(1705)
