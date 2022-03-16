6b61f8e5d6809d4d8b1ad6d9aceb4fe0db623315
James L <59751464+Jazz1016@users.noreply.github.com>
2022-03-16 10:53:18 -0600

    kata lvl 7 completed

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1507,7 +1507,75 @@ func formatMoney(_ val:Double) -> String {
     return "$\(newNum)\(decimalSeenNumbers)"
 }
 
-formatMoney(3)
-formatMoney(3.1)
-formatMoney(30.1)
-formatMoney(3.1431241234)
+//formatMoney(3)
+//formatMoney(3.1)
+//formatMoney(30.1)
+//formatMoney(3.1431241234)
+
+//Definition
+//Jumping number is the number that All adjacent digits in it differ by 1.
+//
+//Task
+//Given a number, Find if it is Jumping or not .
+//
+//Warm-up (Highly recommended)
+//Playing With Numbers Series
+//Notes
+//Number passed is always Positive .
+//
+//Return the result as String .
+//
+//The difference between ‘9’ and ‘0’ is not considered as 1 .
+//
+//All single digit numbers are considered as Jumping numbers.
+
+//Input >> Output Examples
+//jumpingNumber(9) ==> return "Jumping!!"
+//Explanation:
+//It's single-digit number
+//jumpingNumber(79) ==> return "Not!!"
+//Explanation:
+//Adjacent digits don't differ by 1
+//jumpingNumber(23) ==> return "Jumping!!"
+//Explanation:
+//Adjacent digits differ by 1
+//jumpingNumber(556847) ==> return "Not!!"
+//Explanation:
+//Adjacent digits don't differ by 1
+//jumpingNumber(4343456) ==> return "Jumping!!"
+//Explanation:
+//Adjacent digits differ by 1
+//jumpingNumber(89098) ==> return "Not!!"
+//Explanation:
+//Adjacent digits don't differ by 1
+//jumpingNumber(32) ==> return "Jumping!!"
+//Explanation:
+//Adjacent digits differ by 1
+
+func jumpingNumber(_ number: Int) -> String {
+    if number < 10 {
+        return "Jumping!!"
+    }
+    let numStr = "\(number)"
+    var jumpingBool = true
+    for (i, el) in numStr.enumerated() {
+        if i == numStr.count - 1 {
+            break
+        }
+        if (Int(String(el)) ?? 0) + 1 == Int(String(numStr[numStr.index(numStr.startIndex, offsetBy: i + 1)])) ?? 0 || (Int(String(el)) ?? 0) - 1 == Int(String(numStr[numStr.index(numStr.startIndex, offsetBy: i + 1)])) ?? 0  {
+            print("still jumping")
+        } else {
+            jumpingBool = false
+        }
+    }
+    if jumpingBool {
+        return "Jumping!!"
+    } else {
+        return "Not!!"
+    }
+}
+
+//jumpingNumber(1)
+//jumpingNumber(23)
+//jumpingNumber(4345)
+//jumpingNumber(92)
