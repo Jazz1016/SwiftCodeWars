46f96a190d8c48319469e265a9e1ada78f6699ee
James Lea <jameslea@Admins-MacBook-Pro.local>
2023-05-10 11:45:24 -0600

    kata 8

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1677,3 +1677,12 @@ func grow(_ arr: [Int]) -> Int {
   }
   return num
 }
+
+//Make a simple function called greet that returns the most-famous "hello world!".
+//
+//Style Points
+//Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+func greet() -> String {
+  return "hello world!"
+}
