4ecd8090a03d4d240ffa481a95d36e67446abf91
James Lea <james.q.lea@gmail.com>
2023-12-31 21:54:25 -0800

    kata lvl 7

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1788,3 +1788,52 @@ func numbersWithDigitInside(_ x: Int64, _ d: Int64) -> [Int64] {
   return [Int64(arr.count), sum, product]
 }
 
+//My friend John likes to go to the cinema. He can choose between system A and system B.
+//
+//System A : he buys a ticket (15 dollars) every time
+//System B : he buys a card (500 dollars) and a first ticket for 0.90 times the ticket price,
+//then for each additional ticket he pays 0.90 times the price paid for the previous ticket.
+//Example:
+//If John goes to the cinema 3 times:
+//
+//System A : 15 * 3 = 45
+//System B : 500 + 15 * 0.90 + (15 * 0.90) * 0.90 + (15 * 0.90 * 0.90) * 0.90 ( = 536.5849999999999, no rounding for each ticket)
+//John wants to know how many times he must go to the cinema so that the final result of System B, when rounded up to the next dollar, will be cheaper than System A.
+//
+//The function movie has 3 parameters: card (price of the card), ticket (normal price of a ticket), perc (fraction of what he paid for the previous ticket) and returns the first n such that
+//
+//ceil(price of System B) < price of System A.
+//More examples:
+//movie(500, 15, 0.9) should return 43
+//    (with card the total price is 634, with tickets 645)
+//movie(100, 10, 0.95) should return 24
+//    (with card the total price is 235, with tickets 240)
+
+
+func movie(card: Double, ticket: Double, perc: Double) -> Int {
+  print(card, ticket, perc)
+  var aTotal: Double = 0
+  var bTotal: Double = 0
+  var cardCost: Double = 0
+  var counter: Double = 0
+  
+  aTotal -= card
+  cardCost = ticket
+    
+    print(cardCost, aTotal)
+  
+  while aTotal < bTotal {
+      print("hit")
+    counter += 1
+    bTotal -= ticket
+      if counter > 1 {
+          cardCost = cardCost * perc
+      }
+    aTotal -= cardCost
+      print(aTotal, bTotal)
+  }
+  
+  return Int(counter)
+}
+
+movie(card: 5755.0, ticket: 3.0, perc: 0.28)
