8e8642bc932daae40d69cd2b0049d89bd8f81035
James Lea <james.q.lea@gmail.com>
2024-01-02 20:35:05 -0800

    kata lvl 7

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1836,4 +1836,44 @@ func movie(card: Double, ticket: Double, perc: Double) -> Int {
   return Int(counter)
 }
 
-movie(card: 5755.0, ticket: 3.0, perc: 0.28)
+//The Arara are an isolated tribe found in the Amazon who count in pairs. For example one to eight is as follows:
+//
+//1 = anane
+//2 = adak
+//3 = adak anane
+//4 = adak adak
+//5 = adak adak anane
+//6 = adak adak adak
+//7 = adak adak adak anane
+//8 = adak adak adak adak
+//
+//Take a given number and return the Arara's equivalent.
+//
+//e.g.
+//
+//countArara 3 // -> "adak anane"
+//
+//countArara 8 // -> "adak adak adak adak"
+//countArara(3) // "adak anane"
+//
+//countArara(8) // "adak adak adak adak"
+//https://en.wikipedia.org/wiki/Arara_people
+//
+
+
+
+func countArare(_ n: Int) -> String {
+  var string = ""
+    for i in 0...n/2 {
+      if i == 0 {
+        continue
+      }
+      string += "adak "
+    }
+  
+  if n % 2 != 0 {
+    string += "anane"
+  }
+  
+  return string
+}
