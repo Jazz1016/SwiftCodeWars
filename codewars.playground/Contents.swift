10376fc504d54a7296cb87c28847c6ff234e3060
James Lea <james.q.lea@gmail.com>
2024-01-08 09:57:51 -0800

    kata lvl 8

--- codewars.playground/Contents.swift
+++ codewars.playground/Contents.swift
@@ -1992,4 +1992,46 @@ func add(_ n: Int) -> ((Int) -> Int) {
     return fxToReturn(num: )
 }
 
-add(4)(2)
+//add(4)(2)
+
+//Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+//
+//Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+//
+//[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//   7      6      5      4      3            2      1
+//If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+//
+//Note: there will always be exactly one wolf in the array.
+//
+//Examples
+//Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+//Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+//
+//Input: ["sheep", "sheep", "wolf"]
+//Output: "Pls go away and stop eating my sheep"
+//
+
+func warnTheSheep(_ queue: [String]) -> String {
+    print(queue)
+    var counter = 0
+    var wolfLocation = 0
+    var sheepInDanger = 0
+    for i in queue {
+        print(i)
+        if i == "wolf" {
+            wolfLocation = counter
+        }
+        counter += 1
+    }
+    
+    sheepInDanger = queue.count - wolfLocation - 1
+    
+    if wolfLocation == queue.count - 1 {
+        return "Pls go away and stop eating my sheep"
+    } else {
+        return "Oi! Sheep number \(sheepInDanger)! You are about to be eaten by a wolf!"
+    }
+}
+
+warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"])
